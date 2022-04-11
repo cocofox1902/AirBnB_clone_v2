5c8ff86734ee6012f7c8b005b786d95b34cf2f0c
cocofox1902 <colas.renard1902@gmail.com>
2022-04-11 12:18:45 +0000

    add console

--- console.py
+++ console.py
@@ -265,7 +265,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         # first determine if kwargs or args
-        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
+        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) == dict:
             kwargs = eval(args[2])
             args = []  # reformat kwargs into list, ex: [<name>, <value>, ...]
             for k, v in kwargs.items():
