36b6ca7f0e9ca588042bd3948835d75909370ecc
cocofox1902 <colas.renard1902@gmail.com>
2022-04-11 13:20:48 +0000

    add task 6

--- models/city.py
+++ models/city.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import models
 
 
-if models.storage_type == 'db':
-    Base = declarative_base()
-else:
-    Base = object
-
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if models.storage_t == 'db':
-        state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
-        name = Column(String(60), nullable=False)
-        __tablename__ = "cities"
+    if models.storage_t == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        __tablename__ = 'cities'
     else:
         state_id = ""
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ instantiates a new state """
+        super().__init__(*args, **kwargs)
