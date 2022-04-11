af10859721b43466ac163f46f5127c76ce7b3eff
cocofox1902 <colas.renard1902@gmail.com>
2022-04-11 18:36:12 +0000

    add task 8

--- models/user.py
+++ models/user.py
@@ -2,24 +2,16 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from models.base_model import Base
-from os import getenv
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-            email = Column(String(128), nullable=False)
-            password = Column(String(128), nullable=False)
-            first_name = Column(String(128), nullable=False)
-            last_name =  Column(String(128), nullable=False)
-            __tablename__ = 'users'
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
-
-    def __init__(self, *args, **kwargs):
-        """ instantiates a new state """
-        super().__init__(*args, **kwargs)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name =  Column(String(128))
+    __tablename__ = 'users'
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
