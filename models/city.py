1f210e0b2a6d5d1f8a497db57581d2579b0d3e6a
cocofox1902 <colas.renard1902@gmail.com>
2022-04-11 12:24:38 +0000

    add all task

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, ForeignKey, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import models
+
+
+if models.storage_type == 'db':
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if models.storage_t == 'db':
+        state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+        name = Column(String(60), nullable=False)
+        __tablename__ = "cities"
+    else:
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if models.storage_t == 'db':
+        name = Column(String(60), nullable=False)
+        __tablename__ = "cities"
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ instantiates a new state """
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != 'db':
+        @property
+        def cities(self):
+            """ returns list all cities"""
+            list_city = []
+            list_cities_complete = models.storage.all(City)
+            for city in list_cities_complete.values():
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
