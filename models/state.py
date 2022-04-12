7952a152da609b303a3a3dbc7086f2cac98e50ff
cocofox1902 <colas.renard1902@gmail.com>
2022-04-12 08:50:26 +0000

    add state change

--- models/state.py
+++ models/state.py
@@ -11,16 +11,26 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    name = Column(String(60), nullable=False)
-    __tablename__ = "states"
-    cities = relationship("City", backref="state", cascade="delete")
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != 'db':
+    def __init__(self, *args, **kwargs):
+        """state init"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """ returns list all cities"""
-            list_city = []
-            for city in list(models.storage.all(City).values()):
+            """cities of state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+                    city_list.append(city)
+            return city_list
