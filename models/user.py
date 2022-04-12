0bcb5fac2587d84497f8f2d21481c22c1f02a61c
cocofox1902 <colas.renard1902@gmail.com>
2022-04-12 15:21:33 +0000

    add console task 7

--- models/user.py
+++ models/user.py
@@ -16,8 +16,12 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-        places = relationship("Place", backref="User")
-        reviews = relationship("Review", backref="User")
+        places = relationship("Place",
+                               backref="User",
+                               cascade="all, delete, delete-orphan")
+        reviews = relationship("Review",
+                                backref="User",
+                                cascade="all, delete, delete-orphan")
     else:
         email = ""
         password = ""
