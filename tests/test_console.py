2eb9834ad4b1128c4819274902d1fd08e7f3bfdd
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-13 12:10:55 +0000

    add test files

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,175 +1,41 @@
 #!/usr/bin/python3
-"""test for console"""
-import unittest
-from unittest.mock import patch
-from io import StringIO
-import os
-import json
+"""
+Contains the class TestConsoleDocs
+"""
+
 import console
-import tests
-from console import HBNBCommand
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.engine.file_storage import FileStorage
+import inspect
 import pep8
-
-class TestConsole(unittest.TestCase):
-    """Test for console"""
-
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['file1.py', 'file2.py'])
+import unittest
+HBNBCommand = console.HBNBCommand
 
 
-    def setUp(self):
-        """Set up for test"""
-        self.stdout = StringIO()
-        self.stderr = StringIO()
-        self.stdin = StringIO()
-        self.stdin.write('quit')
-        self.stdin.seek(0)
-        self.stdout.seek(0)
-        self.stderr.seek(0)
-        self.hbnb = HBNBCommand()
-        self.hbnb.stdout = self.stdout
-        self.hbnb.stderr = self.stderr
-        self.hbnb.stdin = self.stdin
-        
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
-    def test_create(self):
-        """Test create"""
-        self.hbnb.do_create("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_show(self):
-        """Test show"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_show("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_destroy(self):
-        """Test destroy"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_destroy("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_all(self):
-        """Test all"""
-        self.hbnb.do_create("BaseModel")
-        self.assertEqual(self.stderr.getvalue(), "")
-    
-    def test_update(self):
-        """Test update"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-
-    def test_show_id(self):
-        """Test show_id"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_show("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_destroy_id(self):
-        """Test destroy_id"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_destroy("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id(self):
-        """Test update_id"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-    
-    def test_update_id_no_id(self):
-        """Test update_id_no_id"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr(self):
-        """Test update_id_no_attr"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_value(self):
-        """Test update_id_no_value"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr_value(self):
-        """Test update_id_no_attr_value"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr_value_2(self):
-        """Test update_id_no_attr_value_2"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-    
-    def test_update_id_no_attr_value_3(self):
-        """Test update_id_no_attr_value_3"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-    
-    def test_update_id_no_attr_value_4(self):
-        """Test update_id_no_attr_value_4"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr_value_5(self):
-        """Test update_id_no_attr_value_5"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr_value_6(self):
-        """Test update_id_no_attr_value_6"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr_value_7(self):
-        """Test update_id_no_attr_value_7"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stdout.getvalue(), "")
-        self.assertEqual(self.stderr.getvalue(), "")
-
-    def test_update_id_no_attr_value_8(self):
-        """Test update_id_no_attr_value_8"""
-        self.hbnb.do_create("BaseModel")
-        self.hbnb.do_update("BaseModel")
-        self.assertEqual(self.stderr.getvalue(), "")
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
